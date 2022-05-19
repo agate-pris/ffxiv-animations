9c299a7b7681584be315fd4fbb99c97cad79dbdc
agate-pris <agate-pris@users.noreply.github.com>
2022-05-20 01:34:36 +0900

    new file:   Assets/XivSim/Scripts/SpriteDictionary.cs

---
+++ Assets/XivSim/Scripts/SpriteDictionary.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SpriteDictionary {
+    [SerializeField] List<SpriteDictionaryKeyValuePair> list;
+    public Sprite Get(string key) {
+        foreach (var e in list) {
+            if (e.Key == key) {
+                return e.Value;
+            }
+        }
+        return null;
+    }
+}
