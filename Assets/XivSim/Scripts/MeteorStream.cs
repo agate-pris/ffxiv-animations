f285415cfeedf71a6bbf57e0731df050918b83aa
agate-pris <agate-pris@users.noreply.github.com>
2022-05-20 01:33:53 +0900

    new file:   Assets/XivSim/Scripts/MeteorStream.cs

---
+++ Assets/XivSim/Scripts/MeteorStream.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class MeteorStream : MonoBehaviour, ITimeControl, IPropertyPreview {
+    [SerializeField] GameObject target = default;
+
+    public void OnControlTimeStart() {
+        if (!target) {
+            return;
+        }
+        transform.position = target.transform.position;
+    }
+    public void OnControlTimeStop() { }
+    public void SetTime(double _time) { }
+    public void GatherProperties(PlayableDirector director, IPropertyCollector driver)
+        => driver.AddFromName<Transform>(transform.gameObject, "m_LocalPosition");
+}
