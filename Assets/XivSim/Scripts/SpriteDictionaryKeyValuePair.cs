2f5494a15b0acc17052304a143eec2844f6deb59
agate-pris <agate-pris@users.noreply.github.com>
2022-05-20 01:34:50 +0900

    new file:   Assets/XivSim/Scripts/SpriteDictionaryKeyValuePair.cs

---
+++ Assets/XivSim/Scripts/SpriteDictionaryKeyValuePair.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct SpriteDictionaryKeyValuePair {
+    [SerializeField] string key;
+    [SerializeField] Sprite value;
+
+    public string Key {
+        get => key;
+        set { key = value; }
+    }
+    public Sprite Value {
+        get => value;
+        set { this.value = value; }
+    }
+}
